6c55add368e13912b0e19332cd710b2412f007e5
Themesses <sebastian.gugulski@gmail.com>
2024-03-26 14:56:21 -0400

    module export fix

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /* eslint-disable-next-line no-underscore-dangle */
-const _require = require('esm')(module);
+// const _require = require('esm')(module);
 
-module.exports = _require('./src/index').default;
+// module.exports = _require('./src/index').default;
+module.exports = require("./lib").default;
